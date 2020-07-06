5975e2440bbd63e3ec7deafe5c660ec0fa86f34c
Cornelius Smith <smithcornelius78@gmail.com>
2020-07-06 19:34:28 -0400

    Created an image picker and a basic button to access it through SwiftUI

--- Vision Demo/ContentView.swift
+++ Vision Demo/ContentView.swift
@@ -8,14 +8,31 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
+    @State private var isShowingPicker = false
+    @State private var selectedPhoto = UIImage()
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack{
+            Image(uiImage: self.selectedPhoto)
+            .resizable()
+            .frame(width: 300, height: 300)
+            
+            Button(action:{
+                self.isShowingPicker = true
+            }){
+                Text("Choose Photo")
+            }
+        }
+        .sheet(isPresented: $isShowingPicker) {
+            ImagePicker(source: .photoLibrary, selectedImage: self.$selectedPhoto)
+        }
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
+//struct ContentView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ContentView()
+//    }
+//}
